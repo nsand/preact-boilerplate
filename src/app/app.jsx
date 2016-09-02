80e7d291c42fa9a96a27cbb86d0ab870bdbaa467
Nick Sandonato <nsandona@us.ibm.com>
2016-09-02 14:59:16 -0400

    Add an example of componentDidMount

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,10 +1,18 @@
 import { h, render, Component } from 'preact';
 
 export default class Clock extends Component {
-    render() {
-        let time = new Date().toLocaleTimeString();
-        return <span>{ time }</span>;
-    }
+	constructor() {
+		super();
+		this.setState({time: new Date().toLocaleTimeString()});
+	}
+	componentDidMount() {
+		this.interval = setInterval(() => {
+			this.setState({time: new Date().toLocaleTimeString()});
+		}, 1000);
+	}
+  render({}, {time}) {
+      return <span>{ time }</span>;
+  }
 }
 
 // render an instance of Clock into <body>:
